61729db8e9cb9788cc0fe5800a625c95ca515413
ThienBinh7396 <45134604+ThienBinh7396@users.noreply.github.com>
2020-02-28 15:49:06 +0700

    Update index.js

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = "http://localhost:5000/api/"
-export const BASE_URL = "/api/"
+export const BASE_URL = "http://localhost:5000/api/"
+//export const BASE_URL = "/api/"
 
 export const axios = {
   UPDATE_TOKEN: "UPDATE_TOKEN"
